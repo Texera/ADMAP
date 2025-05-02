108294ce58736320551d552bd987609181efb4cf
Lana Ramadan <lanamramadan@gmail.com>
2025-05-02 10:30:41 -0700

    Removing SCP-related information

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/auth/AuthResource.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/auth/AuthResource.scala
@@ -1,6 +1,5 @@
 package edu.uci.ics.texera.web.resource.auth
 
-import akka.actor.Address
 import edu.uci.ics.amber.engine.common.AmberConfig
 import edu.uci.ics.texera.auth.JwtAuth.{
   TOKEN_EXPIRE_TIME_IN_DAYS,
@@ -10,8 +9,11 @@ import edu.uci.ics.texera.auth.JwtAuth.{
   jwtToken
 }
 import edu.uci.ics.texera.dao.SqlServer
-import edu.uci.ics.texera.web.auth.JwtAuth._
-import edu.uci.ics.texera.web.model.http.request.auth.{LdapUserRegistrationRequest, RefreshTokenRequest, UserLoginRequest, UserRegistrationRequest}
+import edu.uci.ics.texera.web.model.http.request.auth.{
+  RefreshTokenRequest,
+  UserLoginRequest,
+  UserRegistrationRequest
+}
 import edu.uci.ics.texera.web.model.http.response.TokenIssueResponse
 import edu.uci.ics.texera.dao.jooq.generated.Tables.USER
 import edu.uci.ics.texera.dao.jooq.generated.enums.UserRoleEnum
@@ -21,26 +23,9 @@ import edu.uci.ics.texera.web.resource.auth.AuthResource._
 import org.jasypt.util.password.StrongPasswordEncryptor
 
 import javax.ws.rs._
-import javax.ws.rs.core.{MediaType, Response}
-import com.unboundid.ldap.sdk.{AddRequest, Entry, LDAPConnection}
-import com.unboundid.ldap.sdk._
-import com.jcraft.jsch._
-import edu.uci.ics.texera.auth.SessionUser
-import io.dropwizard.auth.Auth
-import play.api.libs.json.Json
-
-import java.security.SecureRandom
-import java.util.Base64
-import com.typesafe.config.{Config, ConfigFactory}
-import edu.uci.ics.amber.engine.common.AmberConfig.{conf, configFile, getConfSource, lastModifiedTime}
-//import edu.uci.ics.texera.web.resource.dashboard.user.metadata.MetadataResource
-
-import java.io.File
+import javax.ws.rs.core.MediaType
 
 object AuthResource {
-  private val configFile: File = new File("src/main/resources/application.conf")
-  private var lastModifiedTime: Long = 0
-  private var conf: Config = ConfigFactory.load()
 
   final private lazy val userDao = new UserDao(
     SqlServer
@@ -49,32 +34,14 @@ object AuthResource {
       .configuration
   )
 
-  private def getConfSource: Config = {
-    if (lastModifiedTime == configFile.lastModified()) {
-      conf.resolve()
-    } else {
-      lastModifiedTime = configFile.lastModified()
-      conf = ConfigFactory.parseFile(configFile).withFallback(ConfigFactory.load())
-      conf.resolve()
-    }
-  }
-
-
-  // constants
-  final private val HOST_IP: String = getConfSource.getString("aws-service.public-ip")
-  final private val PRIVATE_KEY_PATH: String = getConfSource.getString("aws-service.private-key-path")
-  final private val KNOWN_HOSTS: String = getConfSource.getString("aws-service.known-hosts")
-
-
   /**
-    * Retrieve exactly one User from databases with the given username and password.
-    * The password is used to validate against the hashed password stored in the db.
-    *
-    * @param name     String
-    *
-    * @param password String, plain text password
-    * @return
-    */
+   * Retrieve exactly one User from databases with the given username and password.
+   * The password is used to validate against the hashed password stored in the db.
+   *
+   * @param name     String
+   * @param password String, plain text password
+   * @return
+   */
   def retrieveUserByUsernameAndPassword(name: String, password: String): Option[User] = {
     Option(
       SqlServer
@@ -86,125 +53,12 @@ object AuthResource {
         .fetchOneInto(classOf[User])
     ).filter(user => new StrongPasswordEncryptor().checkPassword(password, user.getPassword))
   }
-
-  def generateSecurePassword(length: Int = 8): String = {
-    val random = new SecureRandom()
-    val bytes = new Array[Byte](length)
-    random.nextBytes(bytes)
-    Base64.getUrlEncoder.withoutPadding.encodeToString(bytes).take(length)
-  }
-
-  def addUserToLdap(ldapUser: User): LDAPResult = {
-    val ldapHost = HOST_IP
-    val ldapPort = 389
-    val ldapBindDN = "cn=admin,dc=admap,dc=com"
-    val ldapBindPassword = "23627"
-    val baseDN = "dc=admap,dc=com"
-
-    val emailPrefix = ldapUser.getEmail.split("@")(0)
-    val uid = ldapUser.getUid
-    val name = ldapUser.getName
-
-    val username = s"${emailPrefix}_${uid}"
-    val password = ldapUser.getPassword
-
-    var connection: LDAPConnection = null
-    try {
-      connection = new LDAPConnection(ldapHost, ldapPort, ldapBindDN, ldapBindPassword)
-      val entry = new Entry(s"uid=$username,ou=users,$baseDN")
-
-      entry.addAttribute("objectClass", "inetOrgPerson", "posixAccount", "shadowAccount")
-      entry.addAttribute("uid", username)
-      entry.addAttribute("cn", name)
-      entry.addAttribute("sn", name)
-      entry.addAttribute("userPassword", password)
-      entry.addAttribute("loginShell", "/bin/bash")
-      entry.addAttribute("uidNumber", uid.toString)
-      entry.addAttribute("gidNumber", "5000")
-      entry.addAttribute("homeDirectory", s"/home/users/$username")
-
-      // Use AddRequest explicitly
-      val addRequest = new AddRequest(entry)
-      val result = connection.add(addRequest)
-
-      createHomeDirectory(ldapUser)
-
-      result
-
-    } catch {
-      case e: LDAPException =>
-        println(s"LDAP error: ${e.getMessage}, Code: ${e.getResultCode}")
-        throw e
-
-    } finally {
-      if (connection != null && connection.isConnected) {
-        connection.close()
-      }
-    }
-  }
-
-  /**
-    * Creates a home directory for user's home directory.
-    * The directory will be created at: /home/users/$username
-    * where $username is derived from the user's email and UID.
-    */
-  def createHomeDirectory(ldapUser: User): Boolean = {
-    val emailPrefix = ldapUser.getEmail.split("@")(0)
-    val uid = ldapUser.getUid
-    val username = s"${emailPrefix}_${uid}"
-
-    val path = s"/home/users/$username/"
-
-    val sshHost = HOST_IP
-    val sshUser = "ubuntu"
-    val privateKeyPath = PRIVATE_KEY_PATH
-
-    // make directory and change ownership to the user
-    val command = s"sudo mkdir -p $path && sudo chown -R $username:5000 $path"
-
-    var session: Session = null
-
-    try {
-      val jsch = new JSch()
-      jsch.setKnownHosts(KNOWN_HOSTS)
-      jsch.addIdentity(privateKeyPath, "12345")
-      session = jsch.getSession(sshUser, sshHost, 22)
-      session.setConfig("StrictHostKeyChecking", "no")
-      session.connect()
-      val channel = session.openChannel("exec").asInstanceOf[ChannelExec]
-      channel.setCommand(command)
-      channel.setErrStream(System.err)
-      channel.connect()
-      true
-    } catch {
-      case e: Exception =>
-        println(s"Error: ${e.getMessage}")
-        e.printStackTrace()
-        false
-    } finally {
-      // Ensure the session is disconnected
-      if (session != null && session.isConnected) {
-        session.disconnect()
-      }
-    }
-  }
 }
 
 @Path("/auth/")
 @Consumes(Array(MediaType.APPLICATION_JSON))
 @Produces(Array(MediaType.APPLICATION_JSON))
 class AuthResource {
-
-  @GET
-  @Path("/password")
-  def password(@Auth user: SessionUser): String = {
-    val userFromDb = userDao.fetchOneByEmail(user.getEmail)
-    val password = userFromDb.getPassword
-    if (password == null) {
-      throw new Exception("Password is null")
-    }
-    password
-  }
 
   @POST
   @Path("/login")
@@ -229,7 +83,6 @@ class AuthResource {
   @POST
   @Path("/register")
   def register(request: UserRegistrationRequest): TokenIssueResponse = {
-    println("TESTING")
     if (!AmberConfig.isUserSystemEnabled)
       throw new NotAcceptableException("User System is disabled on the backend!")
     val username = request.username
@@ -240,10 +93,10 @@ class AuthResource {
         val user = new User
         user.setName(username)
         user.setEmail(username)
-        user.setRole(UserRoleEnum.ADMIN)
-        user.setPassword(generateSecurePassword())
+        user.setRole(UserRoleEnum.RESTRICTED)
+        // hash the plain text password
+        user.setPassword(new StrongPasswordEncryptor().encryptPassword(request.password))
         userDao.insert(user)
-        addUserToLdap(user)
         TokenIssueResponse(jwtToken(jwtClaims(user, dayToMin(TOKEN_EXPIRE_TIME_IN_DAYS))))
       case _ =>
         // the username exists already
--- core/gui/src/app/profile/component/profile.component.ts
+++ core/gui/src/app/profile/component/profile.component.ts
@@ -1,7 +1,6 @@
 import { Component } from "@angular/core";
 import { UserService } from "../../common/service/user/user.service";
 import { User } from "../../common/type/user";
-import { FileDirectoryService } from "../../dashboard/service/user/file-directory/file-directory.service";
 
 @Component({
   selector: "texera-profile",
@@ -10,36 +9,11 @@ import { FileDirectoryService } from "../../dashboard/service/user/file-director
 })
 export class ProfileComponent {
   public user: User | undefined;
-  public scpUsername : string | undefined;
-  public scpPassword: string | undefined;
   public showPassword: boolean = false;
-  public host_ip: string;
 
   constructor(
     private userService: UserService,
-    private fileDirectoryService: FileDirectoryService
   ) {
-    this.host_ip = this.fileDirectoryService.host_ip;
     this.user = this.userService.getCurrentUser();
-
-    if (this.user) {
-      this.scpUsername = `${this.user.email.split("@")[0]}_${this.user.uid}`;
-
-      this.userService.getCurrentUserPassword().subscribe(
-        password => {
-          console.log('Password fetched:', password); // Log the password
-          this.scpPassword = password;
-        },
-        error => {
-          console.error('Error fetching password:', error);
-        }
-      );
-    }
-
-
-  }
-
-  togglePasswordVisibility(): void {
-    this.showPassword = !this.showPassword;
   }
 }
